799617cce7df76458d0810f6586185c14a6e2f60
u0105632 <fabian.winkelmann@wago.com>
2023-06-10 10:48:59 +0200

    Add example endpoints

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ app.use(cors());
 app.use(express.static('public'))
 
 app.get('/weather', async (req, res) => {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${req.query.city}&appid=${process.env.API_KEY}`;
+    let city;
+    if (!req.query.city) {
+        city = "Tel Aviv"
+    } else {
+        city = req.query.city
+    }
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${process.env.API_KEY}`;
     try {
         const weatherData = (await axios.get(apiUrl)).data
         res.status(200).json(weatherData);
@@ -21,6 +27,30 @@ app.get('/weather', async (req, res) => {
 
 app.get("/", (req,res) => {
     res.sendFile('public/index.html')  
+})
+
+app.get("/chuckNorris", async (req,res) => {
+    const apiUrl = `https://api.chucknorris.io/jokes/random`;
+    try {
+        const chuckNorrisData = (await axios.get(apiUrl)).data
+        res.status(200).json(chuckNorrisData);
+    } catch {
+        res.status(204)
+    }
+})
+
+app.get('/cat', async (req, res) => {
+    res.sendFile('cat.html', { root: __dirname + '/public' });
+  });
+
+app.get("/todo", async (req,res) => {
+    const apiUrl = `https://jsonplaceholder.typicode.com/todos/1`;
+    try {
+        const todoData = (await axios.get(apiUrl)).data
+        res.status(200).json(todoData);
+    } catch {
+        res.status(204)
+    }
 })
 
 
